ccd1dafaf011387eb6bc2ccea1d9633f1706992e
Gryzulev <granal1@mail.ru>
2026-05-26 23:37:04 +0200

    CORS Middleware

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\EnsureJsonApi;
 use App\Http\Controllers\Api\v1\JokeController;
 use App\Http\Controllers\Api\v1\VsiteursRegister;
+use App\Http\Middleware\CorsMiddleware;
 
 Route::middleware(['api', EnsureJsonApi::class])->prefix('v1')->group(function () {
     Route::get('/jokes', [JokeController::class, 'index']);
-    Route::post('/visiteurs-register', VsiteursRegister::class);
+    Route::post('/visiteurs-register', VsiteursRegister::class)->middleware(CorsMiddleware::class);
 });
